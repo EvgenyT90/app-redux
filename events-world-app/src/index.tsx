37bd8fb0e750c50105350ce132b81c778400bbe0
Evgeny Trapeznikov <evtrapeznikov@omgtu.ru>
2023-10-23 00:59:12 +0600

    Глобальный UserContext

--- events-world-app/src/index.tsx
+++ events-world-app/src/index.tsx
@@ -6,15 +6,18 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { UserProvider } from "./contexts/User.jsx";
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement,
 );
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <UserProvider>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </UserProvider>
         </Provider>
     </React.StrictMode>,
 );
