6b4348fdfe2831c19411a954c23ec6599024101e
Evgeny Trapeznikov <evtrapeznikov@omgtu.ru>
2023-10-23 00:58:38 +0600

    Создание userContext

---
+++ events-world-app/src/contexts/User.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState, useMemo } from "react";
+
+const UserContext = createContext();
+
+const UserProvider = (props) => {
+    const [username, setUsername] = useState("");
+
+    const value = useMemo(() => ({ username, setUsername }), [username]);
+
+    return (
+        <UserContext.Provider value={value}>
+            {props.children}
+        </UserContext.Provider>
+    );
+};
+
+export { UserContext, UserProvider };
--- events-world-app/src/contexts/User.ts
+++
@@ -1,8 +0,0 @@
-import React, { createContext } from "react";
-
-const User: any = {
-    role: "user",
-    name: "",
-};
-
-export const UserContext = createContext(User);
