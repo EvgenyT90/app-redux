b0d98a26f4b2277d856fcfe928d51cb34794884b
Evgeny Trapeznikov <evtrapeznikov@omgtu.ru>
2023-10-23 00:59:35 +0600

    Авторизация теперь проверяется через userContext

--- events-world-app/src/hocs/requireAuth.tsx
+++ events-world-app/src/hocs/requireAuth.tsx
@@ -1,10 +1,14 @@
 import { useLocation, Navigate } from "react-router-dom";
+import { UserContext } from "../contexts/User";
+import { useContext } from "react";
 
 export const RequireAuth = ({ children }: { children: any }) => {
     const location = useLocation();
-    console.log(location);
+    // console.log(location);
+    const { username } = useContext(UserContext);
 
-    const isAuth = localStorage.getItem("auth");
+    const isAuth = username ? true : false;
+    //localStorage.getItem("auth")
 
     if (!isAuth) {
         return <Navigate to="/login" />;
