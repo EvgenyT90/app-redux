d1a98a9dbe0ef1b8e1b7706b28e4e45d385f9997
Evgeny Trapeznikov <evtrapeznikov@omgtu.ru>
2023-10-24 10:40:03 +0600

    prepare rtquery

--- events-world-app/src/store/store.js
+++ events-world-app/src/store/store.js
@@ -1,11 +1,22 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "../reducers";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { usersApi } from "../services/users";
 
-const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(thunk)),
-);
+export const store = configureStore({
+    reducer: {
+        [usersApi.reducerRath]: usersApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(usersApi.middleware),
+});
+// import { createStore, applyMiddleware } from "redux";
+// import thunk from "redux-thunk";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import rootReducer from "../reducers";
 
-export default store;
+// const store = createStore(
+//     rootReducer,
+//     composeWithDevTools(applyMiddleware(thunk)),
+// );
+
+// export default store;
